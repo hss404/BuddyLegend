13f143f09758ecd635abbd8dd59bb58f49f3e617
hss404 <h.sandikli@gmail.com>
2017-07-24 17:52:58 +0200

    new

---
+++ app/src/main/java/com/buddylegend/chat/buddylegend/MainActivity.java
@@ -0,0 +1,13 @@
+package com.buddylegend.chat.buddylegend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
